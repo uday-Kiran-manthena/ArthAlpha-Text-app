87b045bfe24a4cbdf2f3c3e7e90cb32deb819429
uday-Kiran-manthena <udaymanthana07@gmail.com>
2024-09-24 01:28:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import TextStats from "./TextStats";
+
+function App() {
+  return (
+    <div className="App">
+      <TextStats />
+    </div>
+  );
+}
+
+export default App;
